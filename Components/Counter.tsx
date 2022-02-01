94582e78df4ccb8de93e8b13ab1f4c81c26c119c
Šimon Hyksa <simonhyksa6@gmail.com>
2022-02-01 12:51:59 +0100

    Updated packages

--- Components/Counter.tsx
+++ Components/Counter.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react"
+import styled from "styled-components"
 const o = {a:5,b:false,c:6}
 const poleA = [1,2]
 const [a,b] = poleA
 
-
+const StyledBTCounter = styled.button`
+    border: 2px red solid;
+`
+type Props = {
+    className?:string
+}
 
 const pole = [1,2,3]
 const clone = [...pole,4]
@@ -12,8 +18,8 @@ export const Counter:React.FC = () =>{
     const x = useState(1)
     const cislo = x[0];
     const setCislo = x[1]
-    return<>
+    return<div>
     <h1>{cislo}</h1>
-    <button onClick={() => setCislo(previous => previous+1)}>+</button>
-    </>
+    <StyledBTCounter onClick={() => setCislo(previous => previous+1)}>+</StyledBTCounter>
+    </div>
 }
