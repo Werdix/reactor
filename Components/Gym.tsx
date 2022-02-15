2dccfbbb8e76d6b045f0bb28995c2b6b794a7711
Šimon Hyksa <79213930+Werdix@users.noreply.github.com>
2022-02-15 13:09:07 +0100

    Prototype of layout

--- Components/Gym.tsx
+++ Components/Gym.tsx
@@ -1,43 +1,55 @@
 import { FC,useState } from "react"
 import styled from "styled-components"
+import { Slider } from '@mui/material';
 const GymBox = styled.div`
-    border: solid 12px black;
+    border: solid 8px black;
     border-radius: 10px;
     width: 360px;
     height: 240px;
-    margin: 70px 0px 0px 0px; 
-    padding-left:30px;
+    margin: 0px 60px 60px 40px; 
+    padding-left:35px;
+    `
+const RateButton = styled.button`
+width: 55px;
+height: 30px;
+background-color: #F84AA7;
+color: white;
+font-weight: bold;
+font-family: sans-serif;
+border-width: 2px;
+border-radius: 13px;
+border-color: #F84AA7;
 `
 
 export const Gym :FC= () =>{
     
     const[gymDetails,loadedGyms] = useState([{
         id:"1",
-        name:"Suncity",
+        gymName:"Suncity",
         score:1057,
-        countRate:74,
+        countRate:12,
         address:"Praha"
     },{
         id:"2",
-        name:"Gym22",
+        gymName:"Gym22",
         score:1920,
-        countRate:198,
+        countRate:33,
         address:"Hradec Králové"
     },{
         id:"3",
-        name:"MacakGym",
+        gymName:"MacakGym",
         score:1578,
-        countRate: 223,
+        countRate: 22,
         address:"Pardubice"
     }]); 
 
     return(
     <GymBox>
-        <h3>{gymDetails.map(name=>name)}</h3>
-        <h6>Rating: {Math.round((gymDetails.map(score=>score))/(gymDetails.map(countRate => countRate)))}%</h6>
-        <p>Adresa: {gymDetails.map(address=>{address})}</p>
-        <input className="Sl" type="range" min="1" max="100"></input>
-        <label>Your Rating:{}</label>
-        <input type="submit" value="Rate!"></input>
+        <h2>{gymDetails[0].gymName}</h2>
+        <h4>Rating: {Math.round(gymDetails[0].score/gymDetails[0].countRate)}%</h4>
+        <p>Adress: {gymDetails[0].address}</p>
+        <input type="range"></input>
+        <output id="output">Your Rating:{}</output><br/>
+        <RateButton type="button">Rate!</RateButton>
     </GymBox>)
 }
